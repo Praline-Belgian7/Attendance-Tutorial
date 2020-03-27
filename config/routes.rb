11e362901585c1a372759f22ef3553e3bf21aecf
Kento Miyahara <praline-engineering@outlook.com>
2020-03-27 10:54:00 +0000

    6.1 完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   get '/signup' => "users#new"
   resources :users
 
+  #ログイン機能
+  get '/login' => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
