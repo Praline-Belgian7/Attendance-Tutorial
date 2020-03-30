815fca5f740bae1628cb895aed3e319c088c5400
Kento Miyahara <praline-engineering@outlook.com>
2020-03-30 16:18:53 +0000

    8.3 完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,7 @@ class UsersController < ApplicationController
     # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
@@ -58,4 +59,5 @@ class UsersController < ApplicationController
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
     end
+    
 end
