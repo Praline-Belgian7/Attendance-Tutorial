51fe04d19dc8bef5bdb350abcf86cb44b503ad6a
Kento Miyahara <praline-engineering@outlook.com>
2020-04-04 14:38:31 +0000

    8.4 完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def show
@@ -31,6 +31,10 @@ class UsersController < ApplicationController
     else
       render :edit      
     end
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   private
